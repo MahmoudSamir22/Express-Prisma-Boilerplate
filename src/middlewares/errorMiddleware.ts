f33a62f3752908190f7109696b690e65939f796d
Mahmoud Samir <menatosamir55@gmail.com>
2023-08-12 01:21:54 +0300

    handle remove file on error

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,8 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import ApiError from "../utils/ApiError";
 import response from "../utils/response";
+import removeFile from "../utils/fileHandler";
 
 export default (err:ApiError, req:Request, res:Response, next:NextFunction) => {
+    if(req.file){
+        removeFile(req.file.path)
+    }
+    if(req.files){
+        (req.files as Express.Multer.File[]).forEach((file:Express.Multer.File) => {
+            removeFile(file.path)
+        })
+    }
     err.statusCode = err.statusCode || 500;
     if(process.env.NODE_ENV === "development"){
         response(res, err.statusCode, {status: false, message: err.message, stack: err.stack})
--- src/middlewares/joiMiddleware.ts
+++ src/middlewares/joiMiddleware.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 import Joi from "joi";
-import ApiError from "../utils/ApiError";
 
 function joiAsyncMiddleWare(schema: Joi.ObjectSchema<any>) {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -8,13 +7,12 @@ function joiAsyncMiddleWare(schema: Joi.ObjectSchema<any>) {
       const { error, value } = await schema.validateAsync(req.body, {
         abortEarly: false,
       });
-      console.log(error);
+
       if (error) {
-        console.log(error);
-        
         const validationErrors = error.details.map((detail:any) => detail.message);
         return res.status(400).json({ errors: validationErrors });
       }
+      
       next();
     } catch (error) {
       next(error);
